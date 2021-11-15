d709d1c936260b3b93b3e85fb5e80434c3d42707
nikosid <nikosid@gmail.com>
2021-11-15 14:51:07 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Dice from "./components/Dice";
 
 function App() {
+  const [diceOne, setDiceOne] = useState([0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dice-container">
+        <Dice number="1" />
+        <Dice number="2" />
+        <Dice number="3" />
+        <Dice number="4" />
+        <Dice number="5" />
+        <Dice number="6" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Dice.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDiceOne,
+  faDiceTwo,
+  faDiceThree,
+  faDiceFour,
+  faDiceFive,
+  faDiceSix,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Dice = (props) => {
+  const [count, setCount] = useState(0);
+  const icon = getIcon(props.number);
+  const clickHandler = () => {
+    setCount((count) => count + 1);
+  };
+  return (
+    <div className="dice">
+      <FontAwesomeIcon icon={icon} size="4x" onClick={clickHandler} />
+      <span className="dice-number">{count}</span>
+    </div>
+  );
+};
+
+const getIcon = (diceNumber) => {
+  switch (diceNumber) {
+    case "1":
+      return faDiceOne;
+    case "2":
+      return faDiceTwo;
+    case "3":
+      return faDiceThree;
+    case "4":
+      return faDiceFour;
+    case "5":
+      return faDiceFive;
+    case "6":
+      return faDiceSix;
+  }
+};
+
+export default Dice;
