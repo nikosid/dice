54e59b71099c008c7a7606a2b48dc6ac7a1b9f55
nikosid <nikosid@gmail.com>
2021-11-16 15:26:30 +0100

    rename route files

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import OneDice from "./routes/one-dice";
-import TwoDice from "./routes/two-dice";
+import Onedice from "./routes/onedice";
+import Twodice from "./routes/twodice";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="one-dice" element={<OneDice />} />
-        <Route path="two-dice" element={<TwoDice />} />
+        <Route path="one-dice" element={<Onedice />} />
+        <Route path="two-dice" element={<Twodice />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
